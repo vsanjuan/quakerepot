715b5dbde3e154537ed872022767e9d165fa1ee1
salvador.sanjuan@gmail.com <salvador.sanjuan@gmail.com>
2016-10-09 23:50:01 +0200

    Feat: Read minimum magnitude and change the query

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.appindexing.Action;
@@ -67,11 +70,29 @@ public class SettingsActivity extends AppCompatActivity {
         client.disconnect();
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener{
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
 
     }
